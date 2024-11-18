7c52255ae925710a33a579706fb3989e47f01deb
HackersHiveClub <h4ck3rsh1v3@gmail.com>
2024-11-18 07:50:26 -0500

    Hello World Script

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Nosso primeiro script do curso
+DATA=$(date)
+echo "Hello World"
+echo "============================"
+echo "Data de hoje $DATA"
+echo "Diretório atual:$PWD"
+echo "Hostname:$HOSTNAME"
+
